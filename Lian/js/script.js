585ec147e124aca5a303b81d4d8f73d0294a9fca
InLas <inlas645@mail.ru>
2021-07-20 13:04:10 +0300

    Правки

--- Lian/js/script.js
+++ Lian/js/script.js
@@ -6,7 +6,6 @@ const navToogle = document.querySelector('.controls__toogle');
 if (mainNav) {
   mainNav.classList.remove('main-nav--no-js');
   navToogle.classList.remove('controls__toogle--no-js');
-  mainNav.classList.add('main-nav--closed');
 
   navToogle.addEventListener('click', () => {
     navToogle.classList.toggle('controls__toogle--open');
